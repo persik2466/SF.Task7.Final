bb178019fe1b5b22f067e0c01bce9f89d8c9b26f
Irina Lokosova <persik2466@gmail.com>
2021-10-10 17:41:54 +0400

    Добавьте файлы проекта.

---
+++ SF.Task7.Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF.Task7.Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
